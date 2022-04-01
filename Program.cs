321f06f1d17b77add8586c77ef97de5596e77a6e
ErickFPCruz <erick.cruz97@hotmail.com>
2022-04-01 20:55:41 -0300

    Leitor de números / Paridade

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int numero;
+
+Console.Write("Escreva um número qualquer: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+bool numeroEhNegativo = numero < 0;
+
+if (numeroEhNegativo)
+{
+    Console.WriteLine("Você escreveu um número inteiro negativo!");
+}
+Console.WriteLine();
